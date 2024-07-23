0f549e9ae9307f42ec8ef05f2f39d8d2748c2718
joshhovis <joshuahovis3@gmail.com>
2024-07-23 17:12:18 -0400

    Update routes logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import "./App.css";
@@ -10,12 +10,12 @@ import { PrimaryColorProvider } from "./PrimaryColorContext";
 const App = () => {
     return (
         <PrimaryColorProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                </Routes>
-            </Router>
+            {/* <Router> */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+            </Routes>
+            {/* </Router> */}
         </PrimaryColorProvider>
     );
 };
