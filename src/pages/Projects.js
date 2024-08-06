8486e627935c3c3b60393042b68c905f42c45d03
joshhovis <joshuahovis3@gmail.com>
2024-08-06 02:35:31 -0400

    Add new projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,6 +7,8 @@ import bmi from "../assets/bmi.png";
 import newsForm from "../assets/news-form.png";
 import portfolio from "../assets/screenshot-portfolio.png";
 import gamehoc from "../assets/gamehoc.png";
+import tipCalc from "../assets/tip-calculator.png";
+import jmdb from "../assets/jmdb.png";
 
 import { usePrimaryColor } from "../PrimaryColorContext";
 
@@ -23,6 +25,65 @@ const Projects = () => {
             <h1 className="projects__title">Projects</h1>
 
             <div className="projects__list">
+                <div className="projects__list-item">
+                    <img
+                        className="projects__list-img"
+                        src={jmdb}
+                        alt="JMDB home page"
+                    />
+                    <h3 className="projects__list-title">
+                        JMDb (Josh Movie Database)
+                    </h3>
+                    <p className="projects__list-description">
+                        JMDb is a web application built using Next.js and React
+                        that allows users to sign up and login using Clerk
+                        authentication backend API, manage their profiles, and
+                        explore a wide range of movie and TV show content
+                        fetched from the TMDB API. The application allows users
+                        to search, filter, and bookmark their favorite media.
+                        With a focus on navigation and user-friendly design,
+                        this site employs thourough state management to provide
+                        dynamic content loading.
+                    </p>
+                    <div className="projects__list-tools">
+                        <h4 className="projects__list-tools-item">
+                            NextJS / React
+                        </h4>
+                        <h4 className="projects__list-tools-item">
+                            CSS Modules
+                        </h4>
+                        <h4 className="projects__list-tools-item">
+                            Clerk-Auth
+                        </h4>
+                        <h4 className="projects__list-tools-item">OAuth</h4>
+                        <h4 className="projects__list-tools-item">
+                            Axios (API)
+                        </h4>
+                        <h4 className="projects__list-tools-item">
+                            Supabase DB
+                        </h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
+                    </div>
+                    <div className="projects__list-link-row">
+                        <Link
+                            to="https://www.jmdb.site/"
+                            className="projects__list-link"
+                        >
+                            <button className="projects__list-button">
+                                Live Site
+                            </button>
+                        </Link>
+                        <Link
+                            to="https://github.com/joshhovis/Entertainment-App"
+                            className="projects__list-link"
+                        >
+                            <button className="projects__list-button">
+                                Source Code
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+
                 <div className="projects__list-item">
                     <img
                         className="projects__list-img"
@@ -131,6 +192,7 @@ const Projects = () => {
                             express-session
                         </h4>
                         <h4 className="projects__list-tools-item">API</h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
                     </div>
                     <div className="projects__list-link-row">
                         <Link
@@ -143,6 +205,50 @@ const Projects = () => {
                         </Link>
                         <Link
                             to="https://github.com/joshhovis/GameHOC"
+                            className="projects__list-link"
+                        >
+                            <button className="projects__list-button">
+                                Source Code
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="projects__list-item">
+                    <img
+                        className="projects__list-img"
+                        src={tipCalc}
+                        alt="Tip Calculator home page"
+                    />
+                    <h3 className="projects__list-title">Tip Calculator</h3>
+                    <p className="projects__list-description">
+                        A simple user-friendly tip calculator app. Allows users
+                        to input the total bill amount, desired tip percentage,
+                        and the number of people sharing the cost, it then
+                        calculates and displays and both the total amount each
+                        person should pay and the individual tip contributions.
+                    </p>
+                    <div className="projects__list-tools">
+                        <h4 className="projects__list-tools-item">
+                            HTML / SCSS
+                        </h4>
+                        <h4 className="projects__list-tools-item">
+                            JavaScript
+                        </h4>
+                        <h4 className="projects__list-tools-item">Flexbox</h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
+                    </div>
+                    <div className="projects__list-link-row">
+                        <Link
+                            to="https://joshhovis.github.io/Tip-Calculator/"
+                            className="projects__list-link"
+                        >
+                            <button className="projects__list-button">
+                                Live Site
+                            </button>
+                        </Link>
+                        <Link
+                            to="https://github.com/joshhovis/Tip-Calculator"
                             className="projects__list-link"
                         >
                             <button className="projects__list-button">
@@ -174,6 +280,7 @@ const Projects = () => {
                         <h4 className="projects__list-tools-item">
                             Data Visualization
                         </h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
                     </div>
                     <div className="projects__list-link-row">
                         <Link
@@ -215,6 +322,7 @@ const Projects = () => {
                         <h4 className="projects__list-tools-item">
                             Form Validation
                         </h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
                     </div>
                     <div className="projects__list-link-row">
                         <Link
@@ -251,6 +359,7 @@ const Projects = () => {
                         <h4 className="projects__list-tools-item">
                             JavaScript
                         </h4>
+                        <h4 className="projects__list-tools-item">Figma</h4>
                     </div>
                     <div className="projects__list-link-row">
                         <Link
